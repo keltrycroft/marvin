160059a7473831f86ec3ad419838bbb3abb7e972
Kelt Rycroft <kelt.rycroft@gmail.com>
2021-11-10 18:03:21 -0800

    Hello world

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
+    .get('/', (req, res) => res.send('Hello World'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
