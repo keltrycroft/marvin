a563ee007d1210e77a3ea31fb64d4d4021c6cd79
Kelt Rycroft <kelt.rycroft@gmail.com>
2021-11-10 18:08:32 -0800

    Update dependencies

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
     .get('/', (req, res) => res.send('Hello World'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
